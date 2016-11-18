ea2a47426a19c3c0835abcdd0e71ab839b867a76
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-11-17 21:15:26 -1000

    created getHead function

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,10 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var head = null;
+
+  function getHead(){
+    return head;
+  }
 
 }
